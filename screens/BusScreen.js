8b151c5c6cd007d2d3e91e6a5607be38dd22bfdf
Dong Shin <dong_s@me.com>
2017-07-12 01:24:06 +0900

    GPS Module Revoke

--- screens/BusScreen.js
+++ screens/BusScreen.js
@@ -12,44 +12,11 @@ const styles = StyleSheet.create({
   },
 });
 export default class BusScreen extends React.Component {
-  getInitialState ={
-    initialPosition: 'unknown',
-    lastPosition: 'unknown',
-  };
-
-  componentDidMount = () => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const initialPosition = JSON.stringify(position);
-        this.setState({ initialPosition });
-      },
-      (error) => {
-        alert(JSON.stringify(error));
-      }, {
-        enableHighAccuracy: true,
-        timeout: 20000,
-        maximumAge: 1000,
-      });
-    this.watchID = navigator.geolocation.watchPosition((position) => {
-      const lastPosition = JSON.stringify(position);
-      this.setState({ lastPosition });
-    });
-  };
-
-  componentWillUnmount = () => {
-    navigator.geolocation.clearWatch(this.watchID);
-  };
-
   render() {
     return (
       <View>
         <Text>
-          <Text style={styles.title}>Initial position: </Text>
-          {this.state.initialPosition}
-        </Text>
-        <Text>
-          <Text style={styles.title}>Current position: </Text>
-          {this.state.lastPosition}
+          기다리는 중
         </Text>
       </View>
     );
