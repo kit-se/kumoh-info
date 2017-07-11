d9ce4b1d685bc97c7084511a91cd54e81fbadb47
Dong Shin <dongs0104@users.noreply.github.com>
2017-07-12 02:37:29 +0900

    ColorSet, SNS ColorSet 추가, 초기 화면을 Home으로 수정 (#2)
    init commit에 준하는 수정

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -71,5 +71,6 @@ export default TabNavigator(
     tabBarOptions: {
       showLabel: false,
     },
+    initialRouteName: 'Home',
   },
 );
--- screens/BusScreen.js
+++ screens/BusScreen.js
@@ -4,53 +4,103 @@ import {
   StyleSheet,
   Text,
   View,
+  ScrollView,
 } from 'react-native';
+import { Button, Grid, Row, Col, Card } from 'react-native-elements';
+
+import Colors from '../constants/Colors';
+import SocialColors from '../constants/SocialColors';
+
 
 const styles = StyleSheet.create({
   title: {
     fontWeight: '500',
   },
+  Button: {
+    marginTop: 15,
+    paddingTop: 10,
+    width: 100,
+  },
+  contentContainer: {
+    paddingTop: 50,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  gumiStation: {
+    backgroundColor: SocialColors.facebook,
+  },
+  indong: {
+    backgroundColor: SocialColors.instagram,
+  },
+  kit: {
+    backgroundColor: SocialColors.linkedin,
+  },
+  oggye: {
+    backgroundColor: SocialColors.tumblr,
+  },
 });
 export default class BusScreen extends React.Component {
-  getInitialState = () => ({
-    initialPosition: 'unknown',
-    lastPosition: 'unknown',
-  });
-
-  componentDidMount = () => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const initialPosition = JSON.stringify(position);
-        this.setState({ initialPosition });
-      },
-      (error) => {
-        alert(JSON.stringify(error));
-      }, {
-        enableHighAccuracy: true,
-        timeout: 20000,
-        maximumAge: 1000,
-      });
-    this.watchID = navigator.geolocation.watchPosition((position) => {
-      const lastPosition = JSON.stringify(position);
-      this.setState({ lastPosition });
-    });
-  };
-
-  componentWillUnmount = () => {
-    navigator.geolocation.clearWatch(this.watchID);
+  static navigationOptions = {
+    header: null,
   };
 
   render() {
     return (
-      <View>
-        <Text>
-          <Text style={styles.title}>Initial position: </Text>
-          {this.state.initialPosition}
-        </Text>
-        <Text>
-          <Text style={styles.title}>Current position: </Text>
-          {this.state.lastPosition}
-        </Text>
+      <View style={styles.container}>
+        <Grid containerStyle={{ flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignSelf: 'center',
+          paddingTop: 50,
+          backgroundColor: '#95DEE3' }}
+        >
+          <Col size={25} containerStyle={{ marginBottom: 10 }}>
+            <Row>
+              <Button
+                title="구미역으로"
+                backgroundColor={SocialColors.facebook}
+                borderRadius={5}
+                buttonStyle={styles.Button}
+              />
+              <Button
+                title="금오공대로"
+                backgroundColor={SocialColors.linkedin}
+                borderRadius={5}
+                buttonStyle={styles.Button}
+              />
+            </Row>
+            <Row>
+              <Button
+                title="인동으로"
+                backgroundColor={SocialColors.instagram}
+                borderRadius={5}
+                buttonStyle={styles.Button}
+              />
+              <Button
+                title="옥계로"
+                backgroundColor={SocialColors.tumblr}
+                borderRadius={5}
+                buttonStyle={styles.Button}
+              />
+            </Row>
+          </Col>
+          <Row size={75}>
+            <ScrollView>
+              <Card containerStyle={{ marginTop: 15 }} title="1" id="fristCard">
+                <Text style={styles.fonts} h1>h1 Heading</Text>
+              </Card>
+              <Card containerStyle={{ marginTop: 15 }} title="2" id="secondCard">
+                <Text style={styles.fonts} h1>h1 Heading</Text>
+              </Card>
+              <Card containerStyle={{ marginTop: 15 }} title="3" id="thirdCard">
+                <Text style={styles.fonts} h1>h1 Heading</Text>
+              </Card>
+            </ScrollView>
+          </Row>
+        </Grid>
+
       </View>
     );
   }
